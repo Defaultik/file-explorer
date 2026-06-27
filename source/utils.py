c3d593ccb70f8ecedb9540ce821839a19baaf785
Defaultik <defaultik.personal@gmail.com>
2026-06-27 15:41:02 +0300

    Minor changes

--- source/utils.py
+++ source/utils.py
@@ -1,5 +1,6 @@
 TEXT_EXTENSIONS = {".html", ".py", ".js", ".css", ".txt", ".md", ".json", ".xml", ".csv"}
 
+
 def calculate_size(size: int) -> str:
     if size < 1024 ** 2:
         return f"{size / 1024:.2f} KB"
